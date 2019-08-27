916b481ca9fdbb75fba1784889465d7392cca35a
Eduard Barbulescu <eduard.barbulescu@gmail.com>
2019-08-27 17:22:01 +0530

    Update bert_module.py

--- superglue_modules/bert_module.py
+++ superglue_modules/bert_module.py
@@ -33,7 +33,7 @@ class BertLastCLSModule(nn.Module):
         self.dropout = nn.Dropout(dropout_prob)
 
     def forward(self, input):
-        last_hidden = input[-1][(:,), (0,),(:,)]
+        last_hidden = input[-1][:, 0, :]
         out = self.dropout(last_hidden)
         return out
 
