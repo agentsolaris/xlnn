b8a6c24aa5d87d47900160a5bcb65bd2b270f4e4
Eduard Barbulescu <eduard.barbulescu@gmail.com>
2019-08-29 18:37:18 +0530

    Update bert_module.py

--- superglue_modules/bert_module.py
+++ superglue_modules/bert_module.py
@@ -34,7 +34,7 @@ class BertLastCLSModule(nn.Module):
 
     def forward(self, input):
         file1 = open("/content/xlnn/superglue_modules/debug.txt","a") 
-        file1.write(str(input))
+        file1.write(tensor.get_shape().as_list())
         last_hidden = input[0]
     
         out = self.dropout(last_hidden)
