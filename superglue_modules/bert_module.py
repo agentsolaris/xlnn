1edb484437701c6cfb3b252a94e9db7bdc3f59d9
Eduard Barbulescu <eduard.barbulescu@gmail.com>
2019-08-30 16:39:02 +0530

    Update bert_module.py

--- superglue_modules/bert_module.py
+++ superglue_modules/bert_module.py
@@ -21,7 +21,7 @@ class BertModule(nn.Module):
         )
 
     def forward(self, token_ids, token_type_ids=None, attention_mask=None):
-        loss, encoded_layers, pooled_output = self.bert_model(
+        encoded_layers, pooled_output = self.bert_model(
             token_ids, token_type_ids=None,
         )
         return encoded_layers, pooled_output
